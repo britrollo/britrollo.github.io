79d14b827f93462167f74d6486ba535e9e423384
britrollo <britrollo@gmail.com>
2023-09-29 14:03:25 -0400

    created router and error page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import Resume from "./components/Resume";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import { Route, NavLink, Routes } from 'react-router-dom';
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <NavLink to="/resume" target="_self" className={({ isActive, isPending }) =>
+//     isPending ? "pending" : isActive ? "active" : ""
+//   }>Resume</NavLink>
+//           <Routes>
+//             <Route path="/resume/" component={Resume}/>
+//           </Routes>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Redirect } from 'react-router-dom';
+
+class Resume extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome!</h1>
+            </div>
+        )
+    }
+}
+
+export default Resume;
